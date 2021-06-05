c53fd1c3f97a8acd18a1b8fdf031dfce5e289b78
Guilherme Carvalho <guilhermbrsp@gmail.com>
2021-06-04 22:50:41 -0300

    rename first Observer implementation to OOP version

--- src/test/kotlin/behavioral/observer/ObserverOOPTest.kt
+++ src/test/kotlin/behavioral/observer/ObserverOOPTest.kt
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 
-internal class ObserverTest {
+internal class ObserverOOPTest {
 
     private lateinit var eventsManager: EventsManager
 
